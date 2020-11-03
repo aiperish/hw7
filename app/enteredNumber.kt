0348a9f7728381a683e0885c05c6f0963c3f1d09
aipeish <aattokurova02@gmail.ru>
2020-11-03 20:14:14 +0600

    hw7/2
    добавила проект

---
+++ app/enteredNumber.kt
@@ -0,0 +1,199 @@
+ private var enteredNumber = 0
+    private var operand = ""
+    private var default = ""
+    private var lastNumber = ""
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        btn0()
+        btn1()
+        btn2()
+        btn3()
+        btn4()
+        btn5()
+        btn6()
+        btn7()
+        btn8()
+        btn9()
+
+        btnSplit()
+        btnPow()
+        btnPlus()
+        btnMinus()
+
+        btnAC()
+        btnEquals()
+    }
+
+    private fun btn0() {
+        btn_0.setOnClickListener {
+            default = result.text.toString()
+            default += "0"
+            lastNumber += "0"
+            result.text = default
+        }
+    }
+
+    private fun btn1() {
+        btn_1.setOnClickListener {
+            default = result.text.toString()
+            default += "1"
+            lastNumber += "1"
+            result.text = default
+        }
+    }
+
+    private fun btn2() {
+        btn_2.setOnClickListener {
+            default = result.text.toString()
+            default += "2"
+            lastNumber += "2"
+            result.text = default
+        }
+    }
+
+    private fun btn3() {
+        btn_3.setOnClickListener {
+            default = result.text.toString()
+            default += "3"
+            lastNumber += "3"
+            result.text = default
+        }
+    }
+
+    private fun btn4() {
+        btn_4.setOnClickListener {
+            default = result.text.toString()
+            default += "4"
+            lastNumber += "4"
+            result.text = default
+        }
+    }
+
+    private fun btn5() {
+        btn_5.setOnClickListener {
+            default = result.text.toString()
+            default += "5"
+            lastNumber += "5"
+            result.text = default
+        }
+    }
+
+    private fun btn6() {
+        btn_6.setOnClickListener {
+            default = result.text.toString()
+            default += "6"
+            lastNumber += "6"
+            result.text = default
+        }
+    }
+
+    private fun btn7() {
+        btn_7.setOnClickListener {
+            default = result.text.toString()
+            default += "7"
+            lastNumber += "7"
+            result.text = default
+        }
+    }
+
+    private fun btn8() {
+        btn_8.setOnClickListener {
+            default = result.text.toString()
+            default += "8"
+            lastNumber += "8"
+            result.text = default
+        }
+    }
+
+    private fun btn9() {
+        btn_9.setOnClickListener {
+            default = result.text.toString()
+            default += "9"
+            lastNumber += "9"
+            result.text = default
+        }
+    }
+
+    private fun btnEquals() {
+        btn_equals.setOnClickListener {
+            if (operand == "*") {
+                val sum = enteredNumber * lastNumber.toInt()
+                result.text = sum.toString()
+            } else if (operand == "/") {
+                val sum = enteredNumber / lastNumber.toInt()
+                result.text = sum.toString()
+            } else if (operand == "+") {
+                val sum = enteredNumber + lastNumber.toInt()
+                result.text = sum.toString()
+            } else if (operand == "-") {
+                val sum = enteredNumber - lastNumber.toInt()
+                result.text = sum.toString()
+            }
+        }
+    }
+
+    private fun btnSplit() {
+        btn_split.setOnClickListener {
+            default = result.text.toString()
+            default += "/"
+            result.text = default
+        }
+    }
+
+    private fun btnPow() {
+        btn_pow.setOnClickListener {
+            default = result.text.toString()
+            if (isDecimal(default.last())) {
+
+                if (enteredNumber == 0) enteredNumber += lastNumber.toInt()
+                else enteredNumber *= lastNumber.toInt()
+                lastNumber = ""
+                default += "*"
+                result.text = default
+            } else {
+                default = default.dropLast(1)
+                default += "*"
+                result.text = default
+            }
+            operand = "*"
+        }
+    }
+
+    private fun btnPlus() {
+        btn_plus.setOnClickListener {
+            default = result.text.toString()
+            default += "+"
+            result.text = default
+        }
+    }
+
+    private fun btnMinus() {
+        btn_minus.setOnClickListener {
+            default = result.text.toString()
+            default += "-"
+            result.text = default
+        }
+    }
+
+    private fun btnAC() {
+        btn_clear.setOnClickListener {
+            enteredNumber = 0
+            lastNumber = ""
+            result.text = ""
+        }
+    }
+
+    private fun isDecimal(last: Char): Boolean {
+        val charArray = mutableListOf('0', '1', '2', '3', '4', '5', '6', '7', '8', '9')
+
+        for (char in charArray) {
+            if (last == char) return true
+        }
+        return false
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+    }
---
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -0,0 +1,138 @@
+package com.example.calculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private var enteredNumber: Double = 0.0
+    private var operand = ""
+    private var default = ""
+    private var lastNumber = ""
+    private val decimalArrayButtons = mutableListOf<Button>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setDecimalButtonsToArray()
+        setAllButton()
+
+        btnSplit()
+        btnPow()
+        btnPlus()
+        btnMinus()
+
+        btnAC()
+        btnEquals()
+        btnRemoveLast()
+    }
+
+    private fun setDecimalButtonsToArray() {
+        decimalArrayButtons.apply {
+            add(btn_0)
+            add(btn_1)
+            add(btn_2)
+            add(btn_3)
+            add(btn_4)
+            add(btn_5)
+            add(btn_6)
+            add(btn_7)
+            add(btn_8)
+            add(btn_9)
+            add(dot)
+        }
+    }
+
+    private fun setAllButton() {
+        for (btn in decimalArrayButtons) {
+            btn.setOnClickListener {
+                default = result.text.toString()
+                default += btn.text
+                lastNumber += btn.text
+                result.text = default
+            }
+        }
+    }
+
+    private fun btnRemoveLast() {
+        remove.setOnClickListener {
+            if (default.isNotEmpty()) {
+                default = default.dropLast(1)
+                result.text = default
+            }
+        }
+    }
+
+    private fun btnEquals() {
+            btn_equals.setOnClickListener {
+                if (operand == "*") enteredNumber *= lastNumber.toDouble()
+                else if (operand == "/") enteredNumber /= lastNumber.toDouble()
+                else if (operand == "+") enteredNumber += lastNumber.toDouble()
+                else if (operand == "-") enteredNumber -= lastNumber.toDouble()
+                result.text = enteredNumber.toString()
+                lastNumber = ""
+            }
+    }
+
+    private fun btnSplit() {
+        btn_split.setOnClickListener {
+            operandWorker("/")
+        }
+    }
+
+    private fun btnPow() {
+        btn_pow.setOnClickListener {
+            operandWorker("*")
+        }
+    }
+
+    private fun btnPlus() {
+        btn_plus.setOnClickListener {
+            operandWorker("+")
+        }
+    }
+
+    private fun btnMinus() {
+        btn_minus.setOnClickListener {
+            operandWorker("-")
+        }
+    }
+
+    private fun operandWorker(type: String ) {
+        default = result.text.toString()
+        if (isDecimal(default.last())) {
+            //если последний символ цифра
+            if (enteredNumber == 0.0)  enteredNumber += lastNumber.toDouble()
+            lastNumber = ""
+        } else {
+            default = default.dropLast(1)
+        }
+        default += type
+        result.text = default
+        operand = type
+    }
+
+    private fun btnAC() {
+        btn_clear.setOnClickListener {
+            enteredNumber = 0.0
+            lastNumber = ""
+            result.text = ""
+        }
+    }
+
+    private fun isDecimal(last: Char): Boolean {
+        val charArray = mutableListOf('0', '1', '2', '3', '4', '5', '6', '7', '8', '9')
+
+        for (char in charArray) {
+            if (last == char) return true
+        }
+        return false
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+    }
+}
